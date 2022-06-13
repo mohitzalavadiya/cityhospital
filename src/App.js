825ee1575f1624efbaa0783497c4f8b78a887256
mohitzalavadiya <mzpatel14@gmail.com>
2022-06-13 11:11:24 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import About from "./container/departments/About";
+import Contact from "./container/departments/Contact";
 import Departments from "./container/departments/Departments";
 import Doctors from "./container/departments/Doctors";
 import Home from "./container/home/Home";
@@ -14,6 +16,8 @@ function App() {
       <Route path="/" exact component={Home}/>
       <Route path="/departments" exact component={Departments} />
       <Route path="/doctors" exact component={Doctors}/>
+      <Route path ="/about" exact component={About}/>
+      <Route path = "/contact" exact component={Contact}/>
     </Switch>
 
     <Footer/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -40,8 +40,12 @@ function Header(props) {
                                 <li>
                                     <NavLink className="nav-link scrollto" to ="/doctors">Doctors</NavLink>
                                     </li>
-                                <li><a className="nav-link scrollto " href="./pages/about.html">About</a></li>
-                                <li><a className="nav-link scrollto" href="./pages/contact.html">Contact</a></li>
+                                <li>
+                                    <NavLink className="nav-link scrollto " to="/about">About</NavLink>
+                                    </li>
+                                <li>
+                                <NavLink className="nav-link scrollto" to="/contact">Contact</NavLink>
+                                </li>
                             </ul>
                             <i className="bi bi-list mobile-nav-toggle" />
                         </nav>
