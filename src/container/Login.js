5d5822807c17c81f98d8e2e9983267c97b1f3918
mohitzalavadiya <mzpatel14@gmail.com>
2022-06-17 15:34:31 +0530

    yup_formik

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,98 +1,118 @@
 import React, { useState } from 'react';
+import  * as yup from 'yup';
+import { useFormik, Formik, Form } from 'formik';
 
 function Login(props) {
-    const [login, setLogin] = useState("Login")
+  const [login, setLogin] = useState("Login")
 
-    const [reset, setReset] =useState(false)
+  const [reset, setReset] = useState(false)
 
-    return (
-        <section id="appointment" className="appointment">
-  <div className="container">
-    <div className="section-title">
-        {   reset ? 
+  let schema = yup.object().shape({
+    email: yup.string().required("please enter your email id").email("please enter valid email id"),
+    password: yup.string().required("please enter password"),
+  });
+
+  const formikobj = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: schema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  const { handleSubmit, handleChange, errors } = formikobj
+
+
+  return (
+    <section id="appointment" className="appointment">
+      <div className="container">
+        <div className="section-title">
+          {reset ?
             <h2>
-                forget password
+              forget password
             </h2>
             :
             login === "Login" ?
-            <h2>Login</h2>
-            :
-            <h2>signup</h2>
-        }
-     
-      
-    </div>
-    <div action method="post" role="form" className="php-email-form">
-            
+              <h2>Login</h2>
+              :
+              <h2>signup</h2>
+          }
+
+
+        </div>
+        <Formik values={formikobj}>
+          <Form onSubmit={handleSubmit} action method="post" role="form" className="php-email-form">
+
             {
               reset ?
-              null
-              :
-              login === "Login" ?
-              null
-              :
-              <div className="row">
-            <div className="col-md-4 form-group">
-              <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-              <div className="validate" />
-            </div>
-            </div>
-            }
-          {
-            reset ?
-            <div className="row">
-            <div className="col-md-4 form-group mt-3 mt-md-0">
-              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-              <div className="validate" />
-            </div>
-            </div>
-            :
-            <div>
-            <div className="row">
-            <div className="col-md-4 form-group mt-3 mt-md-0">
-              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-              <div className="validate" />
-            </div>
-            </div>
-            <div className="row">
-            <div className="col-md-4 form-group mt-3 mt-md-0">
-              <input type="password" className="form-control" name="password" id="password" placeholder="Your password" />
-              <div className="validate" />
-            </div>
-            </div>
-            </div>
-          }
-        {            
-            login === "Login" ? 
-            <div>
-            Create a new account  <button onClick={() => {setLogin("signup"); setReset(false)}}>Signup</button>
-            <br></br>
-           
-            </div>
-            :
-            <div>
-            Already have an account  <button onClick={() => {setLogin("Login"); setReset(false)}}>Login</button>
-            </div>
-        }
-        <button onClick={()=> {setReset(true)}}>forget password</button>
-       
-        {
-          reset ?
-          <div className="text-center"><button type="submit">submit</button></div>
-          :
-            login === "Login" ?
-            <div className="text-center"><button type="submit">Login</button></div>
-            :
-            <div className="text-center"><button type="submit">Signup</button></div>
-        }
-      
-       
-      
-    </div>
-  </div>
-</section>
+                null
+                :
+                login === "Login" ?
+                  null
+                  :
+                  <div className="row">
+                    <div className="col-md-4 form-group">
+                      <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" />
 
-    );
+                    </div>
+                  </div>
+            }
+            <div className="row">
+              <div className="col-md-4 form-group mt-3 mt-md-0">
+                <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" onChange={handleChange}/>
+                <p>{errors.email}</p>
+
+              </div>
+            </div>
+
+            {
+              reset ?
+                null
+                :
+                <div className="row">
+                  <div className="col-md-4 form-group mt-3 mt-md-0">
+                    <input type="password" className="form-control" name="password" id="password" placeholder="Your password" onChange={handleChange}/>
+                  <p>{errors.password}</p>
+                  </div>
+                </div>
+
+            }
+            {
+              login === "Login" ?
+                <div>
+                  Create a new account  <button onClick={() => { setLogin("signup"); setReset(false) }}>Signup</button>
+                  <br></br>
+
+                </div>
+                :
+                <div>
+                  Already have an account  <button onClick={() => { setLogin("Login"); setReset(false) }}>Login</button>
+                </div>
+            }
+            <button onClick={() => { setReset(true) }}>forget password</button>
+
+            {
+              reset ?
+                <div className="text-center"><button type="submit">submit</button></div>
+                :
+                login === "Login" ?
+                  <div className="text-center"><button type="submit">Login</button></div>
+                  :
+                  <div className="text-center"><button type="submit">Signup</button></div>
+            }
+
+
+
+          </Form>
+        </Formik>
+
+      </div>
+    </section>
+
+  );
 }
 
 export default Login;
